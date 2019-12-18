620de188999b9d9dfcc25af2a1bbef3c9edfe6e6
Modern Viking <thomascustoms715@gmail.com>
2019-12-17 21:29:39 -0700

    styling and moment date, time

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+//display the current date and time at the top of the page
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentTime").text(currentTime);
+//save user input from each text area to local storage
+});
